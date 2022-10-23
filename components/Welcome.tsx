bc7d8eb837203da54f72e2a1557590d8d62b4f3f
nachomglz <nachopad3@gmail.com>
2022-10-23 17:35:52 +0200

    Fixed typing animation on the welcome section

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -3,40 +3,42 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 
 const Welcome: NextPage = () => {
-  const [fullName, setFullName] = useState<string>("Nacho Martin");
-  const [displayName, setDisplayName] = useState<string>("");
+	const [fullName, setFullName] = useState<string>("Nacho\u00A0Martin");
+	const [displayName, setDisplayName] = useState<string>("");
 
-  useEffect(() => {
-    updateName();
-  });
+	useEffect(() => {
+		// Once the component loads, set the displayname to the first letter of the fullName
+		if (displayName.length < fullName.length) {
+			setTimeout(() => {
+				setDisplayName(
+					prev => prev + fullName.substring(prev.length, prev.length + 1)
+				);
+			}, 200);
+		} else {
+			return () => {};
+		}
+	}, [displayName, fullName]);
 
-  const updateName = () => {
-    let interval = setInterval(() => {
-      setDisplayName(prev => fullName.substring(0, prev.length + 1));
-      if (displayName.length === fullName.length) clearInterval(interval);
-    }, 150);
-  };
-
-  return (
-    <section id='Welcome' className='container'>
-      <div className='Welcome-typing-animation'>
-        <div className='hello-message'>
-          Hello,&nbsp;
-          <span className='hello-message-im'>
-            I&apos;m{" "}
-            <span className='hello-message-im-name'>{displayName}</span>
-            <span className='blinker'></span>
-          </span>
-        </div>
-      </div>
-      <div className='Welcome-quote'>
-        <p>I&apos;m a Fullstack developer based in Spain</p>
-      </div>
-      <div className='Welcome-buttons'>
-        <Link href='/#About'>Whoaim</Link>
-        <Link href='/#Contact'>Wanna work with me?</Link>
-      </div>
-    </section>
-  );
+	return (
+		<section id='Welcome' className='container'>
+			<div className='Welcome-typing-animation'>
+				<div className='hello-message'>
+					Hello,&nbsp;
+					<span className='hello-message-im'>
+						I&apos;m{" "}
+						<span className='hello-message-im-name'>{displayName}</span>
+						<div className='blinker'></div>
+					</span>
+				</div>
+			</div>
+			<div className='Welcome-quote'>
+				<p>I&apos;m a Fullstack developer based in Spain</p>
+			</div>
+			<div className='Welcome-buttons'>
+				<Link href='/#About'>Whoaim</Link>
+				<Link href='/#Contact'>Wanna work with me?</Link>
+			</div>
+		</section>
+	);
 };
 export default Welcome;
