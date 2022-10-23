4ceb2fe05ce1962342f60fef4b108057af247026
nachomglz <nachopad3@gmail.com>
2022-10-23 02:01:38 +0200

    Fixed image issue

--- components/About.tsx
+++ components/About.tsx
@@ -7,66 +7,66 @@ import TwitchIcon from "../assets/icons/twitch-icon.svg";
 import YoutubeIcon from "../assets/icons/youtube-icon.svg";
 
 const About: NextPage = () => {
-  return (
-    <section id='About' className='container'>
-      <h1 className='page-title '>Who is Nacho?</h1>
-      <p className='page-subtitle'>
-        I’m a someone who loves building stuff with his computer, and want’s to
-        know everything about them.
-        <br /> I’m constantly trying to challenge myself with new interesting
-        projects, and I love working on them until <br /> they’re finished.
-        <br />
-        <br /> I’m from Spain, and I like to go to a coffe shop a couple of days
-        a week and spend time with my laptop,
-        <br /> It could be thinking about new projects, designing them, or
-        coding them.
-        <br />
-        <br />I also like content creation and post things that I do in my life,
-        sometimes I code on stream and other <br /> times I do surveys to engage
-        my followers to help me decide things for my projects. You can check out
-        <br />
-        my socials, I don’t have anything to hide!
-      </p>
+	return (
+		<section id='About' className='container'>
+			<h1 className='page-title '>Who is Nacho?</h1>
+			<p className='page-subtitle'>
+				I’m a someone who loves building stuff with his computer, and want’s to
+				know everything about them.
+				<br /> I’m constantly trying to challenge myself with new interesting
+				projects, and I love working on them until <br /> they’re finished.
+				<br />
+				<br /> I’m from Spain, and I like to go to a coffe shop a couple of days
+				a week and spend time with my laptop,
+				<br /> It could be thinking about new projects, designing them, or
+				coding them.
+				<br />
+				<br />I also like content creation and post things that I do in my life,
+				sometimes I code on stream and other <br /> times I do surveys to engage
+				my followers to help me decide things for my projects. You can check out
+				<br />
+				my socials, I don’t have anything to hide!
+			</p>
 
-      <div className='about__socials'>
-        <a
-          href='https://instagram.com/nachomglz'
-          target='_blank'
-          rel='noreferrer'
-          className='about__socials-social'
-        >
-          <Image height={1} width={1} src={InstagramIcon} alt='image' />
-        </a>
-        <a
-          href='https://github.com/nachomglz'
-          target='_blank'
-          className='about__socials-social'
-          rel='noreferrer'
-        >
-          <Image height={1} width={1} src={GitHubIcon} alt='image' />
-        </a>
-        <a
-          href='https://linkedin.com/in/ignaciomglz'
-          target='_blank'
-          rel='noreferrer'
-          className='about__socials-social'
-        >
-          <Image height={1} width={1} src={LinkedinIcon} alt='image' />
-        </a>
-        <a
-          href='https://twitch.tv/nachetemartin'
-          target='_blank'
-          rel='noreferrer'
-          className='about__socials-social'
-        >
-          <Image height={1} width={1} src={TwitchIcon} alt='image' />
-        </a>
-        <a className='about__socials-social'>
-          <Image height={1} width={1} src={YoutubeIcon} alt='image' />
-        </a>
-      </div>
-    </section>
-  );
+			<div className='about__socials'>
+				<a
+					href='https://instagram.com/nachomglz'
+					target='_blank'
+					rel='noreferrer'
+					className='about__socials-social'
+				>
+					<Image height={25} width={25} src={InstagramIcon} alt='image' />
+				</a>
+				<a
+					href='https://github.com/nachomglz'
+					target='_blank'
+					className='about__socials-social'
+					rel='noreferrer'
+				>
+					<Image height={25} width={25} src={GitHubIcon} alt='image' />
+				</a>
+				<a
+					href='https://linkedin.com/in/ignaciomglz'
+					target='_blank'
+					rel='noreferrer'
+					className='about__socials-social'
+				>
+					<Image height={25} width={25} src={LinkedinIcon} alt='image' />
+				</a>
+				<a
+					href='https://twitch.tv/nachetemartin'
+					target='_blank'
+					rel='noreferrer'
+					className='about__socials-social'
+				>
+					<Image height={25} width={25} src={TwitchIcon} alt='image' />
+				</a>
+				<a className='about__socials-social'>
+					<Image height={25} width={25} src={YoutubeIcon} alt='image' />
+				</a>
+			</div>
+		</section>
+	);
 };
 
 export default About;
--- components/MySkills.tsx
+++ components/MySkills.tsx
@@ -18,103 +18,103 @@ import NextIcon from "../assets/tagSphereItems/next-icon.svg";
 import MongoIcon from "../assets/tagSphereItems/mongodb-icon.svg";
 
 const tagSphereItems: Array<string | ReactNode> = [
-  <Image src={ReactLogo} key={1} alt='image' />,
-  <Image src={GitIcon} key={2} alt='image' />,
-  <Image src={NextIcon} key={3} alt='image' />,
-  <Image src={MongoIcon} key={4} alt='image' />,
-  <Image src={JavaIcon} key={5} alt='image' />,
-  <Image src={JavaScriptIcon} key={6} alt='image' />,
-  <Image src={NodeIcon} key={7} alt='image' />,
-  <Image src={PhpIcon} key={8} alt='image' />,
-  <Image src={PythonIcon} key={9} alt='image' />,
-  <Image src={TypeScriptIcon} key={10} alt='image' />,
-  <Image src={SassIcon} key={11} alt='image' />,
+	<Image layout='fill' src={ReactLogo} key={1} alt='image' />,
+	<Image layout='fill' src={GitIcon} key={2} alt='image' />,
+	<Image layout='fill' src={NextIcon} key={3} alt='image' />,
+	<Image layout='fill' src={MongoIcon} key={4} alt='image' />,
+	<Image layout='fill' src={JavaIcon} key={5} alt='image' />,
+	<Image layout='fill' src={JavaScriptIcon} key={6} alt='image' />,
+	<Image layout='fill' src={NodeIcon} key={7} alt='image' />,
+	<Image layout='fill' src={PhpIcon} key={8} alt='image' />,
+	<Image layout='fill' src={PythonIcon} key={9} alt='image' />,
+	<Image layout='fill' src={TypeScriptIcon} key={10} alt='image' />,
+	<Image layout='fill' src={SassIcon} key={11} alt='image' />,
 ];
 
 const MySkills: NextPage = () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [skills, setSkills] = useState<Skill[]>([
-    { name: "React", percentage: 95, currentPercentage: 0 },
-    { name: "Node", percentage: 90, currentPercentage: 0 },
-    { name: "Python", percentage: 75, currentPercentage: 0 },
-    { name: "PHP", percentage: 70, currentPercentage: 0 },
-    { name: "Java", percentage: 50, currentPercentage: 0 },
-    { name: "SASS", percentage: 90, currentPercentage: 0 },
-  ]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [skills, setSkills] = useState<Skill[]>([
+		{ name: "React", percentage: 95, currentPercentage: 0 },
+		{ name: "Node", percentage: 90, currentPercentage: 0 },
+		{ name: "Python", percentage: 75, currentPercentage: 0 },
+		{ name: "PHP", percentage: 70, currentPercentage: 0 },
+		{ name: "Java", percentage: 50, currentPercentage: 0 },
+		{ name: "SASS", percentage: 90, currentPercentage: 0 },
+	]);
 
-  useEffect(() => {
-    // console.log(document.querySelectorAll(".bottom-section__skills-skill hr"));
-    if (loading) {
-      setTimeout(() => {
-        let newSkillState: Skill[] = skills.map((value: Skill): Skill => {
-          if (value.currentPercentage < value.percentage) {
-            return { ...value, currentPercentage: value.currentPercentage + 1 };
-          } else {
-            return value;
-          }
-        });
-        // if all the skill's currentPercentages have reached the max percentage, set loading to false
-        if (
-          newSkillState.filter(el => el.currentPercentage === el.percentage)
-            .length === newSkillState.length
-        ) {
-          setLoading(false);
-        }
+	useEffect(() => {
+		// console.log(document.querySelectorAll(".bottom-section__skills-skill hr"));
+		if (loading) {
+			setTimeout(() => {
+				let newSkillState: Skill[] = skills.map((value: Skill): Skill => {
+					if (value.currentPercentage < value.percentage) {
+						return { ...value, currentPercentage: value.currentPercentage + 1 };
+					} else {
+						return value;
+					}
+				});
+				// if all the skill's currentPercentages have reached the max percentage, set loading to false
+				if (
+					newSkillState.filter(el => el.currentPercentage === el.percentage)
+						.length === newSkillState.length
+				) {
+					setLoading(false);
+				}
 
-        setSkills(newSkillState);
-      }, 15);
-    }
-  }, [skills]);
+				setSkills(newSkillState);
+			}, 15);
+		}
+	}, [skills]);
 
-  return (
-    <section id='MySkills' className={`container visible`}>
-      <h1 className='page-title'>What am I good at?</h1>
-      <p className={`page-subtitle `}>
-        I like to work with the JavaScript ecosystem to develop most of my
-        projects, but sometimes I like to experiment with other technologies.
-      </p>
+	return (
+		<section id='MySkills' className={`container visible`}>
+			<h1 className='page-title'>What am I good at?</h1>
+			<p className={`page-subtitle `}>
+				I like to work with the JavaScript ecosystem to develop most of my
+				projects, but sometimes I like to experiment with other technologies.
+			</p>
 
-      <div className='bottom-section'>
-        <div className='bottom-section__skills'>
-          <h2>These are my most used technologies</h2>
-          <div className='bottom-section__skills-container'>
-            {skills.map((value: Skill, index: number) => (
-              <div
-                key={index}
-                className={`bottom-section__skills-skill`}
-                style={{ maxWidth: value.currentPercentage + "%" }}
-              >
-                <div className='skill__top-section'>
-                  <h3>{value.name}</h3>
-                  <span
-                    className={`skill-percentage ${
-                      value.currentPercentage >= 8 ? "shown" : "hidden"
-                    }`}
-                  >
-                    {value.currentPercentage}%
-                  </span>
-                </div>
-                <hr />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className='bottom-section__tagcloud'>
-          <TagSphere
-            radius={
-              typeof window !== "undefined" && window.innerWidth >= 1200
-                ? 270
-                : 150
-            }
-            /*radius={270}*/
-            fullWidth={true}
-            fullHeight={true}
-            texts={tagSphereItems}
-          />
-        </div>
-      </div>
-    </section>
-  );
+			<div className='bottom-section'>
+				<div className='bottom-section__skills'>
+					<h2>These are my most used technologies</h2>
+					<div className='bottom-section__skills-container'>
+						{skills.map((value: Skill, index: number) => (
+							<div
+								key={index}
+								className={`bottom-section__skills-skill`}
+								style={{ maxWidth: value.currentPercentage + "%" }}
+							>
+								<div className='skill__top-section'>
+									<h3>{value.name}</h3>
+									<span
+										className={`skill-percentage ${
+											value.currentPercentage >= 8 ? "shown" : "hidden"
+										}`}
+									>
+										{value.currentPercentage}%
+									</span>
+								</div>
+								<hr />
+							</div>
+						))}
+					</div>
+				</div>
+				<div className='bottom-section__tagcloud'>
+					<TagSphere
+						radius={
+							typeof window !== "undefined" && window.innerWidth >= 1200
+								? 270
+								: 150
+						}
+						/*radius={270}*/
+						fullWidth={true}
+						fullHeight={true}
+						texts={tagSphereItems}
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default MySkills;
